836b6ec4b28c5ecec9f3c58a35110dd42b58bdb2
Thomas Wieck <thomas.wieck@hotmail.co.uk>
2024-07-25 21:37:13 +0100

    Old soundcloud embed

--- src/components/CellAutomate.vue
+++ src/components/CellAutomate.vue
@@ -161,27 +161,12 @@ function createCells() {
   <div style="margin: 0 auto; text-align: center">
     <iframe
       width="100%"
-      height="300"
-      scrolling="yes"
+      height="325"
+      scrolling="no"
       frameborder="no"
       allow="autoplay"
-      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1853197767&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1853197767&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
     ></iframe>
-    <div
-      style="
-        font-size: 10px;
-        color: #cccccc;
-        line-break: anywhere;
-        word-break: normal;
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        font-family: Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans,
-          Garuda, Verdana, Tahoma, sans-serif;
-        font-weight: 100;
-      "
-    >
-    </div>
   </div>
   <div
     style="
