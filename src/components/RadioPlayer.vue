9e9d14d0fccb53426a12ac17876f2e69e0830e35
Thomas Wieck <thomas.wieck@hotmail.co.uk>
2025-01-22 21:27:58 +0000

    Update now playing component to use custom css spinning wheel

--- src/components/RadioPlayer.vue
+++ src/components/RadioPlayer.vue
@@ -1,24 +1,87 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const props = defineProps({
+  url: {
+    type: String,
+    default:
+      "https://stream.firstdozenradio.online/listen/first_dozen/radio.mp3",
+  },
+});
+
+let audio: HTMLAudioElement | null = null;
+let isPlaying = ref(false);
+
+const play = async () => {
+  if (audio && isPlaying.value) {
+    audio.pause();
+  }
+  if (audio && !isPlaying.value) {
+    audio.play();
+  }
+};
+
+onMounted(() => {
+  audio = new Audio(props.url);
+  audio.addEventListener("play", () => {
+    isPlaying.value = true;
+    return isPlaying;
+  });
+  audio.addEventListener("pause", () => {
+    isPlaying.value = false;
+    return isPlaying;
+  });
+});
+</script>
 
 <template>
-  <div class="center">
-    <iframe
-      src="https://stream.firstdozenradio.online/public/first_dozen/embed?theme=dark"
-      frameborder="0"
-      allowtransparency="false"
-      style="
-        min-height: 120px;
-        border: 0;
-        width: 90%;
-        max-height: 125px;
-        border: 0;
-        background-color: black;
-        color: white;
-        padding: 8px;
-        border-radius: 20px;
-      "
-    ></iframe>
+  <div @click="play" class="overlap">
+    <img class="layer1" :class="{ playing: isPlaying }" src="/logo.png" />
+    <img class="layer2" :class="{ playing: isPlaying }" src="/ball.png" />
   </div>
 </template>
 
-<style></style>
+<style>
+.overlap {
+  display: grid;
+}
+
+.layer1,
+.layer2 {
+  grid-column: 1;
+  grid-row: 1;
+  margin: auto;
+  width: 300px;
+  height: 300px;
+}
+
+.layer2 {
+  animation-name: spin;
+  animation-duration: 1500ms;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  animation-play-state: paused;
+  animation-direction: reverse;
+}
+
+.layer1 {
+  animation-name: spin;
+  animation-duration: 10000ms;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  animation-play-state: paused;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.playing {
+  animation-play-state: running;
+}
+</style>
